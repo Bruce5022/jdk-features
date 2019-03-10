7e531015d61327b8e027363aa922173b8824f1ed
syzx-szw <shizhanwei123@126.com>
2019-03-10 13:32:33 +0800

    lambda创建接口对象

--- src/main/java/com/thc/lambda/TestLambda02.java
+++ src/main/java/com/thc/lambda/TestLambda02.java
@@ -1,12 +1,16 @@
 package com.thc.lambda;
 
 import com.thc.service.GreetingService;
+import com.thc.service.MathOperation;
 
+/**
+ * lambda表达式实现自定义接口的使用
+ */
 public class TestLambda02 {
 
     public static void main(String[] args) {
-        test01();
-
+//        test01();
+        test02();
     }
 
 
@@ -17,6 +21,19 @@ public class TestLambda02 {
     public static void test01() {
         GreetingService greetingService = (x) -> System.out.println(x + System.currentTimeMillis() + "!");
         greetingService.sayMessage("你好，");
+    }
+
+    /**
+     * 实现加减乘除的运算
+     */
+    public static void test02() {
+
+        MathOperation addOp = (num1, num2) -> num1 + num2;
+        MathOperation subOp = (num1, num2) -> num1 - num2;
+        MathOperation mulOp = (num1, num2) -> (num1 * num2);
+        MathOperation divOp = (num1, num2) -> (num1 * num2);
+
+        System.out.println(addOp.operation(2, 3));
     }
 
 }
---
+++ src/main/java/com/thc/service/MathOperation.java
@@ -0,0 +1,5 @@
+package com.thc.service;
+
+public interface MathOperation {
+    int operation(int a, int b);
+}
